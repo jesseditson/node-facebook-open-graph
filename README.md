f0d95a18164a478b109137b8917fcaac057f48b2
Jesse Ditson <jesse.ditson@gmail.com>
2012-06-12 15:57:46 -0700

    making this thing

--- README.md
+++ README.md
@@ -1,4 +1,54 @@
 node-facebook-open-graph
 ========================
 
-Open graph actions made easier.
+Ridiculously simple facebook open graph caller.
+
+
+## Usage:
+
+First:
+
+    npm install facebook-open-graph
+
+Then:
+
+    var OpenGraph = require('facebook-open-graph'),
+        openGraph = new OpenGraph('fb_app_namespace')
+
+After that:
+
+**show actions:**
+
+    openGraph.show('some-users-access-token','action',function(err,response){
+      // do stuff
+    })
+
+**publish action:**
+
+    openGraph.publish('some-users-access-token','action','object','some-object-url',function(err,response){
+      // do stuff
+    })
+
+**delete action:**
+
+    openGraph.delete('some-users-access-token','some-action-id',function(err,response){
+      // do stuff
+    })
+
+In all of the above, `err` is a native javascript error or `null`, and response is a native javascript `object` (parsed response from FB). 
+
+## Bonus:
+
+don't like using the word "publish" or whatever? Do what you want, homie:
+
+    openGraph.alias('publish','BIRTH')
+    openGraph.alias('delete','MURDER')
+
+Now you can:
+
+    openGraph.BIRTH('some-users-access-token','action','object','some-object-url',function(err,response){
+      // whatever
+    })
+    openGraph.MURDER('some-users-access-token','some-action-id',function(err,response){
+      // yeah, killed the fucker
+    })
---
+++ opengraph.js
@@ -0,0 +1,89 @@
+// Facebook Open Graph
+// -----
+// Wrapper for facebook open graph calls
+
+// Dependencies
+// -----
+var request = require('request'),
+    querystring = require('querystring')
+
+// Local Variables
+// ---
+var graphUrl = 'https://graph.facebook.com',
+    fb_access_token
+
+// Helpers
+// ---
+
+// get a facebook url using some opengraph options
+var getOpenGraphUrl = function(namespace,user_token,action,objectName,object,callback){
+  var query = {access_token : user_token}
+  if(objectName && object){
+    query[objectName] = object
+  } else {
+    // object is optional, but our callback has moved
+    callback = object
+  }
+  var uri = graphUrl + '/me/' + namespace + ':' + action + "?" + querystring.stringify(query)
+  if(typeof callback === 'function'){
+    callback(uri)
+  } else {
+    return uri
+  }
+}
+
+// parse a facebook response
+// (callback first, as it's curried in)
+var parseResponse = function(callback,err,response){
+  if(err || !response){
+    callback(err || "No response.")
+  } else {
+    try {
+      var body = JSON.parse(response.body)
+      callback(null,body)
+    } catch(e){
+      callback(new Error("Non-JSON response returned."),response.body)
+    }
+  }
+}
+
+// Open Graph Class
+// ---
+var OpenGraph = module.exports = function(namespace){
+  this.namespace = namespace
+}
+
+// **Show**
+OpenGraph.prototype.show = function(user_token,action,callback){
+  var uri = getOpenGraphUrl(this.namespace,user_token,action)
+  request.get({
+    uri : uri
+  },parseResponse.bind(this,callback))
+}
+
+// **Publish**
+OpenGraph.prototype.publish = function(user_token,action,objectName,object,callback){
+  var uri = getOpenGraphUrl(this.namespace,user_token,action),
+      form = { access_token : user_token }
+  form[objectName] = object
+  request.post({
+    uri : uri,
+    form : form
+  },parseResponse.bind(this,callback))
+}
+
+// **Delete**
+OpenGraph.prototype.delete = function(user_token,action_id){
+  var uri = graphUrl + '/{' + action_id + '}?access_token=' + user_token
+  request.delete({
+    uri : uri
+  },parseResponse.bind(this,callback))
+}
+
+// **Alias**
+// just for fun, allow aliasing of commands (in case show, publish, delete doesn't work for you)
+OpenGraph.prototype.alias = function(command,action){
+  if(typeof this[action] == 'function'){
+    this[command] = this[action]
+  }
+}
